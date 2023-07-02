5a58a5ac1a4b596d7908b313288e62bc94167e1d
sonht113 <trongson18101999@gmail.com>
2023-07-02 13:21:11 +0700

    [sonit] 👉 Update api chapter of course

--- src/route/features/chapter/chapter.resolver.ts
+++ src/route/features/chapter/chapter.resolver.ts
@@ -1,8 +1,40 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Resolver, Query, Mutation } from '@nestjs/graphql';
 import { ChapterService } from './chapter.service';
 import { ChapterDto } from './dto/chapter.dto';
+import { CreateChapterDto } from './dto/create-course.dto';
 
 @Resolver(() => ChapterDto)
 export class ChapterResolver {
   constructor(private chapterService: ChapterService) {}
+
+  @Query(() => [ChapterDto])
+  queryAllChapter(@Args('idCourse') idCourse: string): Promise<ChapterDto[]> {
+    return this.chapterService.getAllChapter(idCourse);
+  }
+
+  @Query(() => ChapterDto)
+  queryChapter(@Args('id') id: string): Promise<ChapterDto> {
+    return this.chapterService.getChapterById(id);
+  }
+
+  @Mutation(() => ChapterDto)
+  createChapter(@Args('body') body: CreateChapterDto): Promise<ChapterDto> {
+    return this.chapterService.createChapter(body);
+  }
+
+  @Mutation(() => ChapterDto)
+  updateChapter(
+    @Args('id') id: string,
+    @Args('body') body: CreateChapterDto,
+  ): Promise<ChapterDto> {
+    return this.updateChapter(id, body);
+  }
+
+  @Mutation(() => [ChapterDto])
+  deleteChapter(
+    @Args('id') id: string,
+    @Args('idCourse') idCourse: string,
+  ): Promise<ChapterDto[]> {
+    return this.deleteChapter(id, idCourse);
+  }
 }
--- src/route/features/chapter/chapter.service.ts
+++ src/route/features/chapter/chapter.service.ts
@@ -1,11 +1,74 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { v4 as uuid } from 'uuid';
 import { Chapter } from './entities/chapter.entity';
 import { Repository } from 'typeorm';
+import { CreateChapterDto } from './dto/create-course.dto';
 
 @Injectable()
 export class ChapterService {
   constructor(
     @InjectRepository(Chapter) private chapterRepository: Repository<Chapter>,
   ) {}
+
+  /**
+   * @param idCourse: string
+   * @returns [Chapter]
+   */
+  async getAllChapter(idCourse: string): Promise<Chapter[]> {
+    return this.chapterRepository.find({
+      where: {
+        course: idCourse,
+      },
+    });
+  }
+
+  /**
+   * @param id: string
+   * @returns Chapter
+   */
+  async getChapterById(id: string): Promise<Chapter> {
+    const chapter = await this.chapterRepository.findOne({
+      where: {
+        id,
+      },
+    });
+    if (!chapter) {
+      throw new Error('Chapter not found');
+    }
+    return chapter;
+  }
+
+  /**
+   * @param body: CreateChapterDto
+   * @returns Chapter
+   */
+  async createChapter(body: CreateChapterDto): Promise<Chapter> {
+    return this.chapterRepository.save({
+      id: uuid(),
+      ...body,
+    });
+  }
+
+  /**
+   * @param id: string
+   * @param body: CreateChapterDto
+   * @returns Chapter
+   */
+  async updateChapter(id: string, body: CreateChapterDto): Promise<Chapter> {
+    const chapter = await this.getChapterById(id);
+    Object.assign(chapter, body);
+    return this.chapterRepository.save(chapter);
+  }
+
+  /**
+   * @param id: string
+   * @param idCourse: string
+   * @returns [Chapter]
+   */
+  async deleteChapter(id: string, idCourse: string): Promise<Chapter[]> {
+    const chapter = await this.getChapterById(id);
+    this.chapterRepository.delete(chapter._id);
+    return this.getAllChapter(idCourse);
+  }
 }
